f25081d8d8ed90a8074d8ff23f52d6bb8f35bbfc
Matheus Henrique <matheus.privadotvsmart@gmail.com>
2026-09-02 12:17:44 -0300

    test: adiciona testes unitários para XulambsPizza

--- src/PizzaTest.java
+++ src/PizzaTest.java
@@ -16,5 +16,31 @@ public class PizzaTest {
         //Assert
         assertEquals(4, quantos);
     }
+
+    @Test
+    public void naoAdicionarIngredienteNegativo(){
+        //Arrange
+        Pizza pizza = new Pizza();
+
+        //Act
+        int quantos = 
+            pizza.adicionarIngredientes(-4);
+
+        //Assert
+        assertEquals(0, quantos);
+    }
+
+    @Test
+    public void naoAdicionarIngredienteEmExcesso(){
+        //Arrange
+        Pizza pizza = new Pizza();
+        int quantos = pizza.adicionarIngredientes(4);
+
+        //Act
+        quantos = pizza.adicionarIngredientes(6);
+
+        //Assert
+        assertEquals(4, quantos);
+    }
     
 }
